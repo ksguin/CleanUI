461b0ec1debe925c899f78400835ee7195d8b5ae
Kumar Subhodeep Guin <kumar.subhodeep64@gmail.com>
2021-02-24 20:26:31 +0530

    Total Code Refactor for RealmeUI Changes to be committed: 	deleted:    Disable_list.txt 	modified:   Package_list_description.txt 	deleted:    Uninstall_list.txt 	modified:   debloat.py 	modified:   reinstall_all.py - Removed redundancy, no need of separate Uninstall_list.txt & Disable_list.txt - Modified debloat.py & reinstall_all.py to fetch packages directly from Package_list_description.txt

--- RealmeUI/debloat.py
+++ RealmeUI/debloat.py
@@ -1,43 +1,61 @@
 import subprocess
+import re
+
+#File Source
+Uninstall_list = open('Package_list_description.txt', 'r')
+Lines = Uninstall_list.readlines()
+Uninstall_list.close()
 
 def adb_uninstall(pkg):
+	print("Package: {}".format(pkg))
 	subprocess.call("adb shell pm uninstall --user 0 {}".format(pkg),shell=True)
+	print("----------")
 	return
 	
 def adb_disable(pkg):
-    subprocess.call("adb shell pm disable-user --user 0 {}".format(pkg),shell=True)
-    return
+	subprocess.call("adb shell pm disable-user --user 0 {}".format(pkg),shell=True)
+	print("----------")
+	return
 
-#Uninstall File Source
-Uninstall_list = open('Uninstall_list.txt', 'r')
-Lines = Uninstall_list.readlines()
-print("     ___________________________________________________")
-print("     |     ...Uninstalling the following apps...       |")
-print("     ---------------------------------------------------")
-for line in Lines:
-    line = line.strip()
-    if "".__ne__(line):
-    	adb_uninstall(line)
-    	print("Package: {}".format(line))
-    	print("----------")
-Uninstall_list.close()
+def parse_yield_list(line):
+    #Package name of form com.package.name
+    return re.findall('[a-zA-Z.]+', line)[0]
+    
+def clean_usable_input(Lines):
+	parsed_list=[]
+	for line in Lines:
+		# Strip & replace multiple spaces to one
+		line= re.sub('\s+',' ',line.strip())
+		if not line.startswith('|') and not line.startswith('%'):
+			continue
+		parsed_list.append(parse_yield_list(line))
+	return parsed_list
+    
+def uninstall_disable_fn(parsed_list):
+	disable= False
+	for x in parsed_list:
+		if x.__eq__('Uninstall'):
+			print("\t___________________________________________________")
+			print("\t%     ...Uninstalling the following apps...       %")
+			print("\t---------------------------------------------------")
+			continue
+		elif x.__eq__('Disable'):
+			disable= True
+			print("\t________________________________________________")
+			print("\t%     ...Disabling the following apps...       %")
+			print("\t------------------------------------------------")
+			continue
+		else:
+			if disable == False:
+				adb_uninstall(x)
+			else:
+				adb_disable(x)
+	return
 
-#Disable File Source
-Disable_list = open('Disable_list.txt', 'r')
-Lines = Disable_list.readlines()
-print("     ________________________________________________")
-print("     |     ...Disabling the following apps...       |")
-print("     ------------------------------------------------")
-for line in Lines:
-    line = line.strip()
-    if "".__ne__(line):
-    	adb_disable(line)
-        #output already shows package name
-    	print("----------")
-Disable_list.close()
+#Driver Program
+parsed_list = clean_usable_input(Lines)
+uninstall_disable_fn(parsed_list)
 
-print("\n")
-print("     ______________________________________")
-print("     |     ...Enjoy the Clean UI...       |")
-print("     --------------------------------------")
-print("\n")
+print("\n\t______________________________________")
+print("\t%     ...Enjoy the Clean UI...       %")
+print("\t--------------------------------------\n")
